179c45e294354ed995480d922ef3fdc4c1235253
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-10-21 16:16:37 +0200

    chore: Install the neccessary dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import InvoiceDetailsPage from './features/InvoiceDetailsPage'
+
+function App() {
+  return (
+    <main className='max-w-5xl mx-auto my-8'>
+     <InvoiceDetailsPage /> 
+    </main>
+  )
+}
+
+export default App
---
+++ src/features/InvoiceDetailsPage.jsx
@@ -0,0 +1,11 @@
+import InvoiceInfoHeader from "./InvoiceInfoHeader"
+
+function InvoiceDetailsPage() {
+  return (
+    <div>
+        <InvoiceInfoHeader />
+    </div>
+  )
+}
+
+export default InvoiceDetailsPage
